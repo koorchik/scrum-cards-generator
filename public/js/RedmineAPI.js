ba105d71e2bdcc1a066b785ef50efa2a6dca18b5
koorchik <koorchik@gmail.com>
2014-06-15 12:36:18 +0300

    Tiny refactoring

--- public/js/RedmineAPI.js
+++ public/js/RedmineAPI.js
@@ -10,28 +10,20 @@ var RedmineAPI = (function() {
     RedmineAPI.prototype = {
         getIssueById: function(id) {
             var self = this;
-
             if (!id) return;
 
             return this._fetchIssue(id).then(function(issue) {
-                return new Promise(function(resolve, reject) {
-                    if (issue.parent) {
-                        console.log('LOAD PARENT %s', issue.parent.id);
+                if (!issue.parent) return issue;
 
-                        self._fetchIssue(issue.parent.id).then(function(parentIssue) {
-                            issue.parent = parentIssue;
-                            resolve(issue);
-                        }).catch(reject);
-                    } else {
-                        resolve(issue);
-                    }
+                return self._fetchIssue(issue.parent.id).then(function(parentIssue) {
+                    issue.parent = parentIssue;
+                    return issue;
                 });
             });
         },
 
         _fetchIssue: function(id) {
             var url  = this.url + '/issues/' + id + '.json?include=children';
-
             console.log('FETCHING %s FROM %s', id, url);
 
             return Promise.resolve( $.ajax({
